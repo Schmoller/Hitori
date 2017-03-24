49f4b8e65ae7e9361fbd9650797d787060934e16
Schmoller <steven.schmoll@gmail.com>
2017-03-24 20:53:16 +1100

    Added initial search set generation

--- src/main/java/schmoller/hitori/Solver.java
+++ src/main/java/schmoller/hitori/Solver.java
@@ -29,14 +29,11 @@ public class Solver {
 		duplicates = buildDuplicateSet();
 		search = new HashSet<>();
 		
+		generateInitialSearchSet();
+		
 		// DeBUG
-		for (IndexedNumber dup : duplicates) {
+		for (IndexedNumber dup : search) {
 			dup.getNumber().setState(NumberState.Marked);
-			Set<IndexedNumber> neighbours = getNeighbours(dup);
-			if (!neighbours.isEmpty()) {
-				System.out.println("Neighbours of " + dup + ":");
-				System.out.println(neighbours);
-			}
 		}
 	}
 	
@@ -118,13 +115,84 @@ public class Solver {
 			neighbours.add(baseSet[col + (row + 1) * cols]);
 		}
 		
-		if (cols < cols - 1) {
+		if (col < cols - 1) {
 			neighbours.add(baseSet[(col + 1) + row * cols]);
 		}
 		
 		// TODO: Just dont add it in the first place
 		neighbours.removeIf(n -> !duplicates.contains(n));
 		return neighbours;
+	}
+	
+	private void generateInitialSearchSet() {
+		IndexedNumber[] duplicateArray = new IndexedNumber[baseSet.length];
+		
+		for (IndexedNumber duplicate : duplicates) {
+			duplicateArray[duplicate.getIndex()] = duplicate;
+		}
+		
+		// Check each row
+		for (int row = 0; row < rows; ++row) {
+			for (int col = 0; col < cols; ++col) {
+				IndexedNumber duplicateBase = duplicateArray[col + row * cols];
+				if (duplicateBase == null) {
+					continue;
+				}
+				
+				Set<IndexedNumber> neighboursBase = getNeighbours(duplicateBase);
+				
+				// Check for others of same value
+				for (int col2 = col + 1; col2 < cols; ++col2) {
+					IndexedNumber duplicate = duplicateArray[col2 + row * cols];
+					if (duplicate == null) {
+						continue;
+					}
+					
+					if (duplicateBase.getNumber().getValue() == duplicate.getNumber().getValue()) {
+						// They are the same value, check for definite values
+						Set<IndexedNumber> neighbours = getNeighbours(duplicate);
+						neighbours.retainAll(neighboursBase);
+						
+						// Any remaining must be there as one of these duplicate values must be shaded
+						if (!neighbours.isEmpty()) {
+							search.addAll(neighbours);
+						}
+					}
+				}
+				
+			}
+		}
+		
+		// Check each col
+		for (int col = 0; col < cols; ++col) {
+			for (int row = 0; row < rows; ++row) {
+				IndexedNumber duplicateBase = duplicateArray[col + row * cols];
+				if (duplicateBase == null) {
+					continue;
+				}
+				
+				Set<IndexedNumber> neighboursBase = getNeighbours(duplicateBase);
+				
+				// Check for others of same value
+				for (int row2 = row + 1; row2 < rows; ++row2) {
+					IndexedNumber duplicate = duplicateArray[col + row2 * cols];
+					if (duplicate == null) {
+						continue;
+					}
+					
+					if (duplicateBase.getNumber().getValue() == duplicate.getNumber().getValue()) {
+						// They are the same value, check for definite values
+						Set<IndexedNumber> neighbours = getNeighbours(duplicate);
+						neighbours.retainAll(neighboursBase);
+						
+						// Any remaining must be there as one of these duplicate values must be shaded
+						if (!neighbours.isEmpty()) {
+							search.addAll(neighbours);
+						}
+					}
+				}
+			}
+		}
 	}
 	
 	private void solve(IndexedNumber start) {
